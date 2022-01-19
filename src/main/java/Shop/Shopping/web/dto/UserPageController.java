bdfb602ebd32c5669656ba6d4632b91bf6561211
Donggyu Han <lucky5632@naver.com>
2022-01-19 11:38:10 +0900

    비로그인/로그인 화면 분리

---
+++ src/main/java/Shop/Shopping/web/dto/UserPageController.java
@@ -0,0 +1,29 @@
+package Shop.Shopping.web.dto;
+
+import Shop.Shopping.config.auth.PrincipalDetails;
+import Shop.Shopping.service.ItemService;
+import Shop.Shopping.service.UserPageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class UserPageController {
+    private UserPageService userPageService;
+    private ItemService itemService;
+
+    // 유저페이지
+    @GetMapping("/user/{id}")
+    public String userPage(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        if(principalDetails.getUser().getId() == id){
+            model.addAttribute("user",userPageService.findUser(id));
+            return "user/userPage";
+        }else{
+            return "redirect:/main";
+        }
+    }
+}
